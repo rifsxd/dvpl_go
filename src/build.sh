28c0955b495015fbaafe9fb43873ec5b601fc2ba
RifsxD <rifat.44.azad.rifs@gmail.com>
2023-09-21 18:29:17 +0600

    updated build script to look for OSARCH

--- src/build.sh
+++ src/build.sh
@@ -18,21 +18,47 @@ is_windows() {
     fi
 }
 
+# Function to check if the architecture is amd64
+is_amd64() {
+    if [ "$(uname -m)" == "x86_64" ]; then
+        return 0  # It's amd64
+    else
+        return 1  # It's not amd64
+    fi
+}
+
+# Function to check if the architecture is 32-bit (x86)
+is_x86() {
+    if [ "$(uname -m)" == "i686" ]; then
+        return 0  # It's x86
+    else
+        return 1  # It's not x86
+    fi
+}
+
 # Check if the first argument is "clean" and perform cleaning if needed
 if [ "$1" == "clean" ]; then
+
+    echo ""
     echo "Cleaning build!"
+    echo ""
+
     rm -rf "build"
+
+    echo ""
     echo "Cleaned build!"
+    echo ""
+    
     mkdir "build"
 fi
 
 # Check if the OS is Linux before compiling for Linux
-if is_linux; then
+if is_linux && is_amd64; then
     echo ""
     echo "Compiling main package dvpl_go for linux_amd64."
     echo ""
 
-    go build -o "build/linux_amd64/dvpl_go"
+    GOOS=linux GOARCH=amd64 go build -o "build/linux_amd64/dvpl_go"
     
     echo ""
     echo "Compiled main package dvpl_go for linux_amd64 -> build/linux_amd64/dvpl_go"
@@ -42,7 +68,7 @@ if is_linux; then
     echo "Compiling sub package dvpl_go_cli for linux_amd64."
     echo ""
 
-    go build -o "build/linux_amd64/dvpl_go_cli" "./cli"
+    GOOS=linux GOARCH=amd64 go build -o "build/linux_amd64/dvpl_go_cli" "./cli"
     
     echo ""
     echo "Compiled sub package dvpl_go_cli for linux_amd64 -> build/linux_amd64/dvpl_go_cli"
@@ -52,7 +78,7 @@ if is_linux; then
     echo "Compiling sub package dvpl_go_gui for linux_amd64."
     echo ""
 
-    go build -o "build/linux_amd64/dvpl_go_gui" "./gui"
+    GOOS=linux GOARCH=amd64 go build -o "build/linux_amd64/dvpl_go_gui" "./gui"
     
     echo ""
     echo "Compiled sub package dvpl_go_gui for linux_amd64 -> build/linux_amd64/dvpl_go_gui"
@@ -60,12 +86,12 @@ if is_linux; then
 fi
 
 # Check if the OS is Windows before compiling for Windows
-if is_windows; then
+if is_windows && is_amd64; then
     echo ""
     echo "Compiling main package dvpl_go for windows_amd64."
     echo ""
 
-    go build -o "build/windows_amd64/dvpl_go.exe"
+    GOOS=windows GOARCH=amd64 go build -o "build/windows_amd64/dvpl_go.exe"
     
     echo ""
     echo "Compiled main package dvpl_go for windows_amd64 -> build/windows_amd64/dvpl_go.exe"
@@ -75,7 +101,7 @@ if is_windows; then
     echo "Compiling sub package dvpl_go_cli for windows_amd64."
     echo ""
 
-    go build -o "build/windows_amd64/dvpl_go_cli.exe" "./cli"
+    GOOS=windows GOARCH=amd64 go build -o "build/windows_amd64/dvpl_go_cli.exe" "./cli"
     
     echo ""
     echo "Compiled sub package dvpl_go_cli for windows_amd64 -> build/windows_amd64/dvpl_go_cli.exe"
@@ -85,9 +111,75 @@ if is_windows; then
     echo "Compiling sub package dvpl_go_gui for windows_amd64."
     echo ""
 
-    go build -o "build/windows_amd64/dvpl_go_gui.exe" "./gui"
+    GOOS=windows GOARCH=amd64 go build -o "build/windows_amd64/dvpl_go_gui.exe" "./gui"
     
     echo ""
     echo "Compiled sub package dvpl_go_gui for windows_amd64 -> build/windows_amd64/dvpl_go_gui.exe"
+    echo ""
+fi
+
+# Check if the OS is Linux before compiling for Linux
+if is_linux && is_x86; then
+    echo ""
+    echo "Compiling main package dvpl_go for linux_386."
+    echo ""
+
+    GOOS=linux GOARCH=386 go build -o "build/linux_386/dvpl_go"
+    
+    echo ""
+    echo "Compiled main package dvpl_go for linux_386 -> build/linux_386/dvpl_go"
+    echo ""
+    
+    echo ""
+    echo "Compiling sub package dvpl_go_cli for linux_386."
+    echo ""
+
+    GOOS=linux GOARCH=386 go build -o "build/linux_386/dvpl_go_cli" "./cli"
+    
+    echo ""
+    echo "Compiled sub package dvpl_go_cli for linux_386 -> build/linux_386/dvpl_go_cli"
+    echo ""
+    
+    echo ""
+    echo "Compiling sub package dvpl_go_gui for linux_386."
+    echo ""
+
+    GOOS=linux GOARCH=386 go build -o "build/linux_386/dvpl_go_gui" "./gui"
+    
+    echo ""
+    echo "Compiled sub package dvpl_go_gui for linux_386 -> build/linux_3864/dvpl_go_gui"
+    echo ""
+fi
+
+# Check if the OS is Windows before compiling for Windows
+if is_windows && is_x86; then
+    echo ""
+    echo "Compiling main package dvpl_go for windows_386."
+    echo ""
+
+    GOOS=windows GOARCH=386 go build -o "build/windows_386/dvpl_go.exe"
+    
+    echo ""
+    echo "Compiled main package dvpl_go for windows_386 -> build/windows_386/dvpl_go.exe"
+    echo ""
+    
+    echo ""
+    echo "Compiling sub package dvpl_go_cli for windows_386."
+    echo ""
+
+    GOOS=windows GOARCH=386 go build -o "build/windows_386/dvpl_go_cli.exe" "./cli"
+    
+    echo ""
+    echo "Compiled sub package dvpl_go_cli for windows_386 -> build/windows_386/dvpl_go_cli.exe"
+    echo ""
+    
+    echo ""
+    echo "Compiling sub package dvpl_go_gui for windows_386."
+    echo ""
+
+    GOOS=windows GOARCH=386 go build -o "build/windows_386/dvpl_go_gui.exe" "./gui"
+    
+    echo ""
+    echo "Compiled sub package dvpl_go_gui for windows_386 -> build/windows_386/dvpl_go_gui.exe"
     echo ""
 fi
